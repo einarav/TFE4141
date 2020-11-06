4c391f27e1e0e73033086f53a9606017b620680c
einar <einaravdem@gmail.com>
2020-11-06 08:57:38 +0100

    Testbench added

--- tfe4141_rsa_integration_kit_2020/Exponentiation/testbench/exponentiation_tb.vhd
+++ tfe4141_rsa_integration_kit_2020/Exponentiation/testbench/exponentiation_tb.vhd
@@ -10,33 +10,63 @@ end exponentiation_tb;
 
 
 architecture expBehave of exponentiation_tb is
+    
+    --DUT ports
+    --valid_in    : std_logic;
+    --ready_out   : std_logic;
+    --message     : std_logic_vector (C_block_size-1 downto 0);
+    --key         : std_logic_vector (C_block_size-1 downto 0);
+            
+    --Clock
+    signal clk 			: STD_LOGIC := '1';
+    constant clk_freq   : integer := 100e6; --100MHz
+    constant clk_period : time := 1000 ms / clk_freq;
+    
+    
 
+    --Inputs
+    signal valid_in 	: STD_LOGIC;
+    signal ready_out 	: STD_LOGIC;  
 	signal message 		: STD_LOGIC_VECTOR ( C_block_size-1 downto 0 );
 	signal key 			: STD_LOGIC_VECTOR ( C_block_size-1 downto 0 );
-	signal valid_in 	: STD_LOGIC;
+	
+	--Outputs
 	signal ready_in 	: STD_LOGIC;
-	signal ready_out 	: STD_LOGIC;
 	signal valid_out 	: STD_LOGIC;
 	signal result 		: STD_LOGIC_VECTOR(C_block_size-1 downto 0);
+	
+	--Internal
 	signal modulus 		: STD_LOGIC_VECTOR(C_block_size-1 downto 0);
-	signal clk 			: STD_LOGIC;
 	signal restart 		: STD_LOGIC;
 	signal reset_n 		: STD_LOGIC;
 
 begin
+ 
+    --Device under test (DUT)
 	i_exponentiation : entity work.exponentiation
-		port map (
-			message   => message  ,
-			key       => key      ,
-			valid_in  => valid_in ,
-			ready_in  => ready_in ,
-			ready_out => ready_out,
-			valid_out => valid_out,
-			result    => result   ,
-			modulus   => modulus  ,
-			clk       => clk      ,
-			reset_n   => reset_n
-		);
+	port map (
+		message   => message  ,
+		key       => key      ,
+		valid_in  => valid_in ,
+		ready_in  => ready_in ,
+		ready_out => ready_out,
+		valid_out => valid_out,
+		result    => result   ,
+		modulus   => modulus  ,
+		clk       => clk      ,
+		reset_n   => reset_n
+	);
+		
+	--Clock generation
+    clk <= not clk after clk_period / 2;
 
-
+    --Testbench sequence
+    process is
+    begin
+        wait for 10 ns;
+        reset_n <= '1';
+        wait for 20 ns;
+        valid_in <= '1';
+        ready_in <= '1';
+    end process;
 end expBehave;
